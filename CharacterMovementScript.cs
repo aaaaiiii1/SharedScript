0a7ff5e57c6d0b35c95fb9869686df21c2c9325f
Vinh Quang <hell967452@gmail.com>
2021-03-08 16:15:29 +0700

    new update
    new update

---
+++ CharacterMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+  
+
+    //NavMeshAgent variable control player movement
+    public NavMeshAgent playerNavMeshAgent;
+    //A Camera that follow player movement
+    public Camera playerCamera;
+    // Update is called once per frame
+    private void Update()
+    {
+        //if the left button of is clicked
+        if (Input.GetMouseButton(0))
+        {
+            //Unity cast a ray from the position of mouse cursor on-screen toward the 3D scene.
+            Ray myRay = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit myRaycastHit;
+
+            if (Physics.Raycast(myRay, out myRaycastHit))
+            {
+                //Assign ray hit point as Destination of Navemesh Agent (Player)
+                playerNavMeshAgent.SetDestination(myRaycastHit.point);
+            }
+        }
+    }
+}
---
+++ camcontrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camcontrol : MonoBehaviour
+    
+{
+    //camera follow variable
+    public Transform playerTransform;
+    private Vector3 _camoffset;
+    [Range(0.01f, 1.0f)]
+    public float smoothness = 0.5f;
+
+    //zoom camera variable
+    private Camera cam;
+    private float targetZoom;
+    private float zoomFactor = 3f;
+    [SerializeField] private float zoomLerpSpeed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _camoffset = transform.position - playerTransform.position;
+        cam = Camera.main;
+        targetZoom = cam.orthographicSize;
+    }
+    private void LateUpdate()
+    {
+        Vector3 newPositio = playerTransform.position + _camoffset;
+        transform.position = Vector3.Slerp(transform.position, newPositio, smoothness);
+    }
+    private void Update()
+    {
+        useWheel();
+    }
+    public void useWheel()
+    {
+        float scrollData;
+        scrollData = Input.GetAxis("Mouse ScrollWheel");
+        targetZoom -= scrollData * zoomFactor;
+        targetZoom = Mathf.Clamp(targetZoom, 2f, 8f);
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
+    }
+}
